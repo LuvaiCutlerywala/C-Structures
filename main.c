f4286f8436e14cb770b7c9bea2755cfd15151a52
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2023-04-23 15:36:28 +0100

    Updated the testing method for stacks
    Added tests to see whether pushing and popping from stacks works. Currently, the methods do not work.

--- main.c
+++ main.c
@@ -8,10 +8,13 @@
 
 void demoQueue();
 void demoLinkedList();
+void demoStacks();
 
-int main(void) { 
-  demoLinkedList(); 
+int main() { 
+  demoStacks();
+  //demoLinkedList(); 
   //demoQueue();
+  return 0;
 }
 
 void demoQueue() {
@@ -72,6 +75,21 @@ void demoStacks() {
                      "Jackfruit", "Cherry", "Pineapple", "Kiwi Fruit"};
 
   Stack *stack = s_generate(8);
-  printf("Stack size: %d", stack->size);
-  printf("Stack max size: %d", stack->maxSize);
+  printf("Stack size: %d\n", stack->size);
+  printf("Stack max size: %d\n", stack->maxSize);
+
+  printf("Pushing items onto stack.\n");
+  for(int i = 0; i < length(strings, char*); ++i){
+    printf("Pushing %s onto stack.\n", strings[i]);
+    s_push(stack, strings[i]);
+    printf("Stack size: %d\n", stack->size);
+    printf("Stack capacity remaining: %d\n", s_capacity(stack));
+  }
+
+  printf("Popping items from the stack...\n");
+  for(int i = stack->size; i >= 0; i--){
+    printf("Popping %s from the stack.", s_pop(stack));
+    printf("Stack size: %d\n", stack->size);
+    printf("Stack capacity remaining: %d\n", s_capacity(stack));
+  }
 }
